fde2f6a4c6f2b3955b6d744b06aac8ee206e0faf
carllb <carllb52@gmail.com>
2018-04-20 15:21:45 -0400

    fix3

--- mojoco_test.py
+++ mojoco_test.py
@@ -46,7 +46,7 @@ for i in range(0,NUM_GUYS):
 while True:
 
         for i in range(0,NUM_GUYS):
-            threads[i] = threading.Thread(target=run_guy, args=(guys[i], envs[i]))
+            threads[i] = threading.Thread(target=run_guy, args=(guys_params[i], envs[i]))
 
         for i in range(0,NUM_GUYS):
             rewards[i] = threads[i].join()
