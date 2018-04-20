70893c67afe8f19872bd0292c7ff570ea6b38cdc
Carl Brando <carllb52@gmail.com>
2018-04-20 15:28:26 -0400

    fix

--- mojoco_test.py
+++ mojoco_test.py
@@ -33,9 +33,9 @@ def run_guy(env,params):
 def mutate(params,rate):
     return params*(1-rate) + np.random.rand(*params.shape)*rate
 
-guys_params = [None] * 5
-envs        = [None] * 5
-threads     = [None] * 5
+guys_params = [None] * NUM_GUYS
+envs        = [None] * NUM_GUYS
+threads     = [None] * NUM_GUYS
 rewards     = np.zeros(NUM_GUYS)
 for i in range(0,NUM_GUYS):
     guys_params[i] = np.random.rand(8,111)
